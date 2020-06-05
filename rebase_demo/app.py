2e1b6a0e5c15ed6b3c7209828384e64c07821edd
Sam Lahti <sam.lahti@avant.com>
2020-06-05 12:48:41 -0500

    Forgot to print args

--- rebase_demo/app.py
+++ rebase_demo/app.py
@@ -5,12 +5,13 @@ def new_cool_subfuntion_for_printing(what_to_print):
     pc = PrintingClass("Coming from app.new_cool_subfuntion_for_printing", what_to_print)
     pc.print()
 
+
 def new_function(args=None):
     new_cool_subfuntion_for_printing(args)
 
 
 def app():
-    #TODO: I know bob will be adding in the arg parsing, working on other cool function for now
+    # TODO: I know bob will be adding in the arg parsing, working on other cool function for now
     new_function()
 
 
