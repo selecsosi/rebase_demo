0f58ab77583069726efbf7c93f1b48dd02fd9fb0
Sam Lahti <sam.lahti@avant.com>
2020-06-05 12:43:32 -0500

    Adding in new cool function for printing while waiting for bob's arg parsing

--- rebase_demo/app.py
+++ rebase_demo/app.py
@@ -1,8 +1,13 @@
-def new_function():
+
+def new_cool_subfuntion_for_printing(what_to_print):
     return "Hello"
+
+def new_function(args=None):
+    new_cool_subfuntion_for_printing(args)
 
 
 def app():
+    #TODO: I know bob will be adding in the arg parsing, working on other cool function for now
     new_function()
 
 
